ab5c650cd94cc5e8b5fd750077c580ce84cb8395
Diego Diaz <ddiaz042202@gmail.com>
2023-09-09 15:41:17 -0400

    cost according to national average

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
 // popup.js
 
 const total_to_wattage = 0.012; // in kWh
+const national_wattage = 0.23; // cost in dollars per kWh
 
 function updatePopup() {
   chrome.runtime.sendMessage({ action: "getWebpageActivity" }, (response) => {
     total_repeats = response.total;
     document.getElementById('text_total').innerHTML = `Tabs Re-opened: ${total_repeats}`
     document.getElementById('text_energy_wasted').innerHTML = `Approximate Energy Wasted ${total_repeats*total_to_wattage} kwH`
+    document.getElementById('text_energy_wasted').innerHTML = `Approximate Cost $${total_repeats*total_to_wattage/national_wattage}`
     console.log(response.total)
   });
 }
