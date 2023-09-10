27bda9d6a6ebbbffc5697d3da9e9b85cfcff81f5
stlgolfer <stlgolfer@users.noreply.github.com>
2023-09-09 23:54:52 -0400

    Should be mostly ok for production

--- popup.js
+++ popup.js
@@ -4,16 +4,20 @@ const total_to_wattage = 0.012; // in kWh
 const national_wattage = 0.23; // cost in dollars per kWh
 
 function updatePopup() {
-  chrome.runtime.sendMessage({ action: "getWebpageActivity" }, (response) => {
-    total_repeats = response.total;
-    document.getElementById('text_total').innerHTML = `Tabs Re-opened: ${total_repeats}`
-    document.getElementById('text_energy_wasted').innerHTML = `Approximate Energy Wasted ${total_repeats*total_to_wattage} kwH`
-    cost = total_repeats*total_to_wattage/national_wattage
-    document.getElementById('cost_energy_wasted').innerHTML = `Approximate Cost $${cost.toFixed(2)}`
-    console.log(response.total)
+    chrome.runtime.sendMessage({action: "getWebpageActivity"}, (response) => {
+        total_repeats = response.total;
+        document.getElementById('text_total').innerHTML = `Links Re-opened: ${total_repeats}`
+        document.getElementById('text_energy_wasted').innerHTML = `Approximate Energy Wasted: ${(total_repeats * total_to_wattage).toFixed(2)} kwH`
+        cost = total_repeats * total_to_wattage * national_wattage
+        document.getElementById('cost_energy_wasted').innerHTML = `Approximate Cost: $${cost.toFixed(2)}`
+        console.log(response.total)
 
-    console.log(`top: ${response.top_site}, top usage: ${response.top_site_usage}`)
-  });
+        //${response.top_site_usage*total_to_wattage*national_wattage}
+        document.getElementById("top_site").innerText = `Most Visited Site: $${(response.top_site_usage*total_to_wattage*national_wattage).toFixed(2)}`
+        document.getElementById("top_site").href = response.top_site
+
+        console.log(`top: ${response.top_site}, top usage: ${response.top_site_usage}`)
+    });
 }
 
 updatePopup();
